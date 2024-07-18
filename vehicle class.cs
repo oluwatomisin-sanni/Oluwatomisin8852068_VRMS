b134dd1556e513eb2f6e9a1d3c37ba79b3a56eb4
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-18 10:01:46 -0400

    creating the vehicle class

---
+++ vehicle class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace VRMS;
+
+public abstract class Vehicle
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public Vehicle(string make, string model, int year)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+    }
+}
