4dbc357972f4bac110787534060ac870cad9b9c4
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-19 17:36:58 -0400

    creating rental agency class

---
+++ Oluwatomisin Sanni VRMS/RentalAgency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VRMS
+{
+    public class RentalAgency
+    {
+        private List<Vehicle> Fleet { get; set; }
+        public double TotalRevenue { get; private set; }
+
+        public RentalAgency()
+        {
+            Fleet = new List<Vehicle>();
+            TotalRevenue = 0;
+        }
+    }
+
