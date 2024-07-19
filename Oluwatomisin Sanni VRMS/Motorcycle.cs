9057794243e0b48bccfdf8bb02c6a7d708ba6dc8
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-19 16:42:24 -0400

    creating the Motorcycle class

---
+++ Oluwatomisin Sanni VRMS/Motorcycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace VRMS
+{
+    public class Motorcycle : Vehicle
+    {
+        public int EnigineCapacity { get; set; }
+        public string FuelType { get; set; }
+        public bool HasFairing { get; set; }
+
+        public Motorcycle(string model, string manufacturer, double rentalPrice, int year, int engineCapacity, string fuelType, bool hasFairing) : base(model, manufacturer, rentalPrice, year)
+        {
+            EngineCapacity = engineCapacity;
+            FuelType = fuelType;
+            HasFairing = hasFairing;
+        }
+
+        public override void DisplayDetails()
+        {
+
+            Console.WriteLine("This Vehicle is a Car");
+            base.DisplayDetails();
+            Console.WriteLine($"Capacity: {EngineCapacity}");
+            Console.WriteLine($"Truck Type: {FuelType}");
+            Console.WriteLine($"Four-Wheel Drive ?: {(HasFairing ? "Yes" : "No")}");
+
+        }
+    }
+
+}
