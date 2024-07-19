23f2c8580d680f73dfdaa390e2526e8f74153e96
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-19 15:46:47 -0400

    creating the Truck class

---
+++ Oluwatomisin Sanni VRMS/Truck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace VRMS
+{
+    public class Truck : Vehicle
+    {
+        public int Capacity { get; set; }
+        public string TruckType { get; set; }
+        public bool FourWheelDrive { get; set; }
+        
+        public Truck(string model, string manufacturer, double rentalPrice, int year, int capacity, string truckType, bool fourWheelDrive) : base(model, manufacturer, rentalPrice, year)
+        {
+            Capacity = capacity;
+            TruckType = truckType;
+            FourWheelDrive = fourWheelDrive;
+        }
+
+        public override void DisplayDetails()
+        {
+
+            Console.WriteLine("This Vehicle is a Car");
+            base.DisplayDetails();
+            Console.WriteLine($"Capacity: {Capacity}");
+            Console.WriteLine($"Truck Type: {TruckType}");
+            Console.WriteLine($"Four-Wheel Drive ?: {(FourWheelDrive ? "Yes" : "No")}");
+
+        }
+    }
+
+}
