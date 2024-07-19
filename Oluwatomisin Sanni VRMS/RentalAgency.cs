1bffc3000a96c813086ac4afa1db53002e947d16
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-19 18:47:03 -0400

    created the rental agencyclass

--- Oluwatomisin Sanni VRMS/RentalAgency.cs
+++ Oluwatomisin Sanni VRMS/RentalAgency.cs
@@ -13,16 +13,47 @@ namespace VRMS
             Fleet = new List<Vehicle>();
             TotalRevenue = 0;
         }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+
+            if (vehicle != null)
+            {
+                Fleet.Add(vehicle);
+                Console.WriteLine($"\n{vehicle.Manufacturer}, {vehicle.Model} has been added\n");
+            }
+        }
+
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+            if (Fleet.Contains(vehicle))
+            {
+                Fleet.Remove(vehicle);
+                Console.WriteLine($"\n{vehicle.Manufacturer}, {vehicle.Model} has been removed\n");
+            }
+        }
+
+        public void RentVehicle(Vehicle vehicle, int days)
+        {
+            if (Fleet.Contains(vehicle))
+            {
+                double rentalCost = vehicle.RentalPrice * days;
+                TotalRevenue += rentalCost;
+                Console.WriteLine($"\n{vehicle.Manufacturer}, {vehicle.Model} has been rented.\nFee:{vehicle.rentalCost}\n");
+            }
+            else
+            {
+                Console.WriteLine("Vehicle not available in the fleet.");
+            }
+        }
+        public void DisplayTotalRevenue()
+        {
+            Console.WriteLine($"Total Revenue: ${TotalRevenue}");
+        }
+
     }
 
-    public void AddVehicle(Vehicle vehicle)
-    {
-       
-        if (vehicle != null)
-        {
-            vehicles.Add(vehicle);
-            Console.WriteLine($"\n{vehicle.Manufacturer}, {vehicle.Model} has been added\n");
-        }
-    }
+    
+
 }
 
