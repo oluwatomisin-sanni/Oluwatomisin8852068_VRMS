eed375254f49e5b52a7b6560d50fbd0bf6e4769f
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-19 21:29:33 -0400

    TESTING

--- Oluwatomisin Sanni VRMS/program.cs
+++ Oluwatomisin Sanni VRMS/program.cs
@@ -1,49 +1,50 @@
 ﻿using System;
 using VRMS;
 namespace VRMS
-
-public class Program
 {
-    static void Main(string[] args)
+    public class Program
     {
-        
-        RentalAgency agency = new RentalAgency();
+        static void Main(string[] args)
+        {
 
-        // instantiating the vehicles and their attributes
-       
-        Car car1 = new Car("Model 3", "Tesla", 80.0, 2023, 5, "Electric", "Automatic", false);
-        Truck truck1 = new Truck("F-150", "Ford", 90.0, 2021, 2000, "Pickup", true);
-        Motorcycle motorcycle1 = new Motorcycle("Ninja", "Kawasaki", 50.0, 2022, 1000, "Gasoline", true);
+            RentalAgency agency = new RentalAgency();
 
-        // Add the instantiated vehicles to the fleet
-        agency.AddVehicle(car1);
-        agency.AddVehicle(truck1);
-        agency.AddVehicle(motorcycle1);
+            // instantiating the vehicles and their attributes
 
-        // Display details of each vehicle
-        Console.WriteLine("\nVehicle Details:");
-        car1.DisplayDetails();
-        Console.WriteLine();
-        truck1.DisplayDetails();
-        Console.WriteLine();
-        motorcycle1.DisplayDetails();
-        Console.WriteLine();
+            Car car1 = new Car("Model 3", "Tesla", 80.0, 2023, 5, "Electric", "Automatic", false);
+            Truck truck1 = new Truck("F-150", "Ford", 90.0, 2021, 2000, "Pickup", true);
+            Motorcycle motorcycle1 = new Motorcycle("Ninja", "Kawasaki", 50.0, 2022, 1000, "Gasoline", true);
 
-        // Renting vehicles
-        Console.WriteLine("\nRenting Vehicles:");
-        agency.RentVehicle(car1, 3);
-        agency.RentVehicle(motorcycle1, 2);
+            // Add the instantiated vehicles to the fleet
+            agency.AddVehicle(car1);
+            agency.AddVehicle(truck1);
+            agency.AddVehicle(motorcycle1);
 
-        // Displaying the total revenue
-        Console.WriteLine("\nAgency Revenue:");
-        agency.DisplayTotalRevenue();
+            // Display details of each vehicle
+            Console.WriteLine("\nVehicle Details:");
+            car1.DisplayDetails();
+            Console.WriteLine();
+            truck1.DisplayDetails();
+            Console.WriteLine();
+            motorcycle1.DisplayDetails();
+            Console.WriteLine();
 
-        // Remove a vehicle
-        Console.WriteLine("\nRemoving a Vehicle:");
-        agency.RemoveVehicle(car1);
+            // Renting vehicles
+            Console.WriteLine("\nRenting Vehicles:");
+            agency.RentVehicle(car1, 3);
+            agency.RentVehicle(motorcycle1, 2);
 
-        // Try to rent the removed vehicle
-        Console.WriteLine("\nAttempting to Rent Removed Vehicle:");
-        agency.RentVehicle(motorcycle1, 1);
+            // Displaying the total revenue
+            Console.WriteLine("\nAgency Revenue:");
+            agency.DisplayTotalRevenue();
+
+            // Remove a vehicle
+            Console.WriteLine("\nRemoving a Vehicle:");
+            agency.RemoveVehicle(car1);
+
+            // Try to rent the removed vehicle
+            Console.WriteLine("\nAttempting to Rent Removed Vehicle:");
+            agency.RentVehicle(motorcycle1, 1);
+        }
     }
 }
